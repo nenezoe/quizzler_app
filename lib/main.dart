7d202d1448d4bc2399350b7e2ae0e62aa6b4f50b
nenezoe <okerekenene7@gmail.com>
2021-07-23 15:47:48 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:quizzler_app/ui/quizzler.dart';
+import 'package:quizzler_app/widgets/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
